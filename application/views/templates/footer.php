6c29d6533b546f8a8fe3f3ac7211df27aa02451e
BayuKun28 <bhayou.essega@gmail.com>
2020-01-16 10:04:33 +0700

    fix sidebar

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -46,7 +46,7 @@
 <script src="<?= base_url('assets/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url('assets'); ?>js/sb-admin-2.min.js"></script>
+<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
 </body>
 
